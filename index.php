dc5790a393dee90661b9115b8da0c7d091802465
Dev-patel1882 <dev.patel@vivanwebsolution.com>
2023-03-27 17:07:08 +0530

    add frontend prac file

---
+++ index.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+	<title>Yoga Om</title>
+</head>
+
+<body>
+	<!-- First Line -->
+  		<div class="line"></div>
+  		<!-- end line -->
+    <div class="page_wrapper">
+  		
+			<!-- header start -->
+  		  <div class="header">
+  		  	 <div class="logo">
+  		  	 	<img src="images/logo_03.png" class="logo" style="width: auto;height: 50px;">
+  		  	 </div>
+  		  	 <div class="links">
+  		  	 	
+		            <li><a class="active" href="#home">Home</a></li>
+		            <li><a href=""><b>Class</b></a></li>
+		            <li><a href="">Promo</a></li>
+		           <li><a href="">Online Class</a></li>
+	               <li><a href="">Contact</a></li>
+  		  	 </div>
+  		  </div>
+         
+         <!-- header end -->
+         
+
+
+         <!-- banner area start -->
+            <div class="main-banner">
+               <div class="center-banner">
+               	 <div class="banner-text"> 
+               	 	<h1>Healthy&nbsp;<span>&</span>&nbsp;Fresh</h1>
+               	 	<br>
+               	 	<br>
+               	 	<br>
+               	 	<strong>YOGA</strong>
+               	 	<br>
+               	    <br>
+               	    <br>
+               	    <br>
+               	    <p>Lorem Ipsum is simply dummy text of <br>the printing and typesetting industry. <br>Lorem Ipsum has been the  </p>
+               	 </div>
+               </div>	
+            </div> 
+             
+         <!-- banner area end -->
+
+
+        <!-- First Section -->
+       <div class="first-sec">
+         	 <span>MEET OUR</span>
+         	 <h1>YOGA CLASSES</h1>
+         </div>
+       <!-- end -->
+  
+       <div class="row-meet">
+		  <div class="column-meet" >
+		    <img src="images/34917082_03.jpg" class="meet-sec-img"> 
+		    <h1 class="dot-line">...</h1>
+		    <br>
+		    <strong>Hatha</strong>
+		    <p>Lorem Ipsum is simply dummy text of ...</p>
+		    
+		  
+             <hr class="meet-hr">
+            <h1>Monday&nbsp;<span>20:00 hs</span></h1>
+		    
+		  </div>
+		  <div class="column-meet" >
+		  	<img src="images/34917082_03.jpg" class="meet-sec-img"> 
+		    <h1 class="dot-line">...</h1>
+		    <br>
+		    <strong>Ashtanga</strong>
+		    <p>Lorem Ipsum is simply dummy text of ...</p>
+		    <hr class="meet-hr">
+            <h1>Tuesday&nbsp;<span>20:00 hs</span></h1>
+		  </div>
+		  <div class="column-meet">
+		  	 <img src="images/34917082_03.jpg" class="meet-sec-img"> 
+		    <h1 class="dot-line">...</h1>
+		    <br>
+		    <strong>Kundalini</strong>
+		    <p>Lorem Ipsum is simply dummy text of ...</p>
+		    <hr class="meet-hr">
+            <h1>Friday&nbsp;<span>20:00 hs</span></h1>
+		  </div>
+		</div>
+
+        <div class="meet-btn">
+        	<button class="btn">SHOW MORE</button>
+        </div>
+       <!-- Second section -->
+          <div class="second-sec">
+         	 <div class="second-sec-img">
+         	 	<img src="images/34917081_03.png" class="sec-img">
+         	 	 
+         	 </div>
+         	 <div class="second-sec-text">
+         	   <strong>SUMMER</strong>
+         	   <h3>CLASS</h3>
+         	   <br><br>
+         	   <p>22.12.2020</p>
+         	    <p>24.03.2021</p>	 	
+         	 </div>
+         </div>
+       <!-- end -->
+
+
+
+
+         
+
+              <div class="row-third">
+				  <div class="column-third">
+				  	<div class="sec-1"> 
+				      	    <img src="images/34917083_02.png" class="third-img-1"/>
+				      </div>
+				      <div class="sec-2">
+				      	 <h2><b style="font-weight: bold;"> PRACTICE</b> <br>DYNAMICS</h2>
+					     
+					         <br><br><br>
+					         <h5 style="font-weight:bold;">1. HATHA <span style="font-weight:lighter;">YOGA<span></h5>
+					         <br>
+					         <p>Hatha is the most classic yoga. its origin <br>is in the raya Yoga or Patanjali and <br>other classical texts such <br>as Bagvad Gita and Hatha Yoga <br> Pradipika.</p>
+					         <br>
+					        <hr style="width:50%;" align="left">
+					        <br>
+					        <h6>10 modules</h6>
+					        <h6>divided into 7 weekends</h6>
+					        <br><br>
+					        <p>Start&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Price</p>
+					        <p>April 2015&nbsp;&nbsp; $900</p>
+					        <p>20:00 hr</p>
+				      </div>
+				     
+				 </div>
+				  <div class="column-third">
+				      <img src="images/34917082_03.jpg" class="third-img-2" />
+                     <h4 class="third-state-line" style="">HATHA</h4>
+				  </div>
+				</div>
+
+
+
+
+               <div class="row-forth">
+				  <div class="column-forth">
+				     <img src="images/34917082_03.jpg" class="forth-img-1" />
+                     <h4 class="forth-state-line" style="">HATHA</h4>
+				 </div>
+				  <div class="column-forth">
+				      <div class="sec-1-forth"> 
+				      	    <img src="images/34917083_03.png" class="forth-img-2"/>
+				      </div>
+				      <div class="sec-2-forth">
+				      	 <h2><b style="font-weight: bold;"> PRACTICE</b> <br>DYNAMICS</h2>
+					     
+					         <br><br><br>
+					         <h5 style="font-weight:bold;">2. KUNDALINI <span style="font-weight:lighter;">YOGA</span></h5>
+					         <br>
+					         <p>Kundalani is the most classic yoga.its <br>origin is in Raya Yoga of Patanjali<br>and other classical texts such as <br>Bhagavad gita and Hatha Yoga Pradipika<br> Pradipika.</p>
+					         <br>
+					        <hr style="width:50%;" align="left">
+					        <br>
+					        <h6>17 modules</h6>
+					        <h6>divided into 5 weekends</h6>
+					        <br><br>
+					        <p>Start&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Price</p>
+					        <p>April 2015&nbsp;&nbsp; $900</p>
+					        <p>20:00 hr</p>
+				      </div>
+				  </div>
+				</div>
+
+                <br>
+
+
+               <div class="row-hel">
+  <div class="column-hel" style="width: 40%;">
+   <hr class="hel-hr">
+  </div>
+  <div class="column-hel" style="width: 20%;">
+    <h2>HEALTHY</h2>
+    <p>LIFE</p>
+  </div>
+  <div class="column-hel" style="width: 40%;">
+   <hr class="hel-hr">
+  </div>
+</div>
+
+
+
+
+              <div class="row-fifth">
+				  <div class="column-fifth">
+				  	  <img src="images/34917083_0311_03.png" class="fifth-img"/>
+				      <br>
+				      <br>
+                      <br>
+
+				     <strong>EXERCISE</strong>
+				     <br><br><br><br>
+				     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+				  </div>
+				  <div class="column-fifth">
+				  	  <img src="images/34917083_0311_03.png"  class="fifth-img"/>
+				  	  <br>
+				      <br>
+                      <br>
+				  	  
+				    <strong>FEEDING</strong>
+				    <br><br><br><br>
+				     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+				  </div>
+				  <div class="column-fifth">
+				  	  <img src="images/34917083_0311_03.png"  class="fifth-img"/>
+				  	  <br>
+				      <br>
+                      <br>
+				     <strong>MEDITATION</strong>
+				     <br><br><br><br>
+				     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+				  </div>
+				</div>
+              </div>
+
+               
+               <div class="six-heading">
+               	   <h3>LOOK AT OUR</h3>
+               	   <br>
+               	   <strong>SUMMER CLASSES</strong>
+               </div>
+
+
+         <div class="page_wrapper">
+         	
+         
+               <div class="row-six">
+				  <div class="column-six">
+				    <img src="images/34917082_03.jpg" class="six-img" />
+				    <br>
+				    <h2>...</h2>
+				    <h1>Anusara</h1>
+				    <br>
+				    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+				    <hr class="six-hr" align="left">
+				    <h4>Friday <span>20:00 hr</span></h4>
+				  </div>
+				  <div class="column-six" >
+				  	<img src="images/34917082_03.jpg" class="six-img" />
+				  	<br>
+				  	<h2>...</h2>
+				    <h1>Kripalu</h1>
+				    <br>
+				   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+				   <hr class="six-hr" align="left">
+				   <h4>Tuesday <span>18:00 hr</span></h4>
+				  </div>
+				  <div class="column-six" >
+				  	<img src="images/34917082_03.jpg" class="six-img" />
+				  	<br>
+				  	<h2>...</h2>
+				    <h1>Bharata</h1>
+				    <br>
+				   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+				    <hr class="six-hr" align="left">
+				    <h4>Sunday<span>&nbsp;13:30 hr</span></h4>
+				  </div>
+				</div>
+
+
+                <br>
+				<div class="meet-btn">
+        	       <button class="btn">SHOW MORE</button>
+                </div>
+
+
+              <div class="row-seven">
+				  <div class="column-seven">
+				  	<div class="seven-sec1">
+				  		<h5 class="seven-state-line-left">INITIATION</h5>
+				  	</div>
+				  	<div class="seven-sec2">
+				  		<h1 class="seven-main-heading"> YOGA INITIATION</h1>
+				    <h1 class="seven-sub-heading">WORKSHOP</h1>
+                     <br><br>
+				    <span class="seven-sub3">TRY YOUR FIRST FREE</span>
+				    <br>
+				    <span class="seven-sub4">YOGA CLASSES</span>
+                     <br>  
+                      <br>  
+                       <br>  
+                        <br>   <br>  
+				    <span class="sub-5">22.12.2020</span>
+				     <br>  
+				    <span class="sub-5">24.03.2021</span>
+				  	</div>
+				    
+				    
+				  </div>
+				  <div class="column-seven">
+				  	<div class="seven-sec2-left">
+				  		<img src="images/34917082_03.jpg" class="saven-img" />
+				  	</div>
+				  	<div class="seven-sec3-left">
+				  		<h1>WORKSHOP</h1>
+				  	</div>
+				    
+				  </div>
+				</div>
+
+                  
+
+               <div class="footer">
+               	  <h2>THE ASANAS PURIFY OUR BODY AND KEEP IT <br>HEAL THY , MAKING IT A SUITABLE VEHICLE FOR <br>THE SOUL <br><br><br><span class="f-sp">"</span></h2>
+               	 
+               	 <div class="row">
+					  <div class="column" style=" width: 55%;">
+					    <h3>About</h3>
+				         <br>
+				         <p>The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s</p>
+
+				         <img src="images/logo_03.png" class="footer-logo">
+					  </div>
+					  <div class="column" style=" width: 15%;">
+					
+				         <h1>About</h1>
+				         <br>
+				         <p>Team</p>
+				         <p>Join us</p>
+				         <p>Ethic</p>
+
+					  </div>
+					  <div class="column" style=" width: 15%;">
+					    <h1>About</h1>
+				         <br>
+				         <p>Team</p>
+				         <p>Join us</p>
+				         <p>Ethic</p>
+				         <p>Goals</p>
+
+					  </div>
+					  <div class="column" style=" width: 15%;">
+					    <h1>About</h1>
+				         <br>
+				         <p>Team</p>
+				         <p>Join us</p>
+				         <p>Ethic</p>
+				         <p>Goals</p>
+					  </div>
+					</div>
+               </div>
+         
+
+
+</div>
+     
+    <!-- last Line -->
+  		<div class="line" style="margin-top:14%;"></div>	
+  		<!-- end line -->
+
+
+
+
+</body>
+</html>
